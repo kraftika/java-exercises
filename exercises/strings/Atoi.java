3ad3430d127152af3a4be66f4acb6357d08f7c9f
Ionut Pistol <ipistol@adobe.com>
2016-05-17 13:27:37 +0300

    Atoi problem

---
+++ exercises/strings/Atoi.java
@@ -0,0 +1,63 @@
+package strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Atoi {
+
+	public static void main(String[] args) {
+		String[] numbers = {"", "   ", "+", "-", "++1", "-1", "-0", "1", "--3", "+1",
+							"0", "1a", "a1", "+a", "-a", "abc", 
+							"2147483647", "2147483648", "2147483649", "214748sa3648",
+							"-2147483648", "-2147483647", "-2147483649", "-2147483753", "-21474833322",
+							"010", "    010", " 010 30"};
+		
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.format("%s -> %d\n", numbers[i], stringToInteger(numbers[i]));
+		}
+	}
+	
+	public static int stringToInteger(String str) {
+		boolean signed = false;
+		
+		str = str.trim();
+		if (str.length() == 0 || 
+			(str.length() == 1 && (str.charAt(0) == '-' || str.charAt(0) == '+'))) {
+			return 0;
+		}
+		
+		if (str.length() >= 1 && (str.charAt(0) == '-' || str.charAt(0) == '+')) {
+			signed = str.charAt(0) == '-';
+			str = str.substring(1);
+		}
+		
+//        for (int i = 0; i < str.length(); i++) {
+//            if ((int)str.charAt(i) < 48 || (int)str.charAt(i) > 57) {
+//               return 0; 
+//            }
+//        }
+		
+		if (str.length() == 1 && str.charAt(0) == '0') {
+			return 0;
+		}
+		
+		double result = 0;
+		for (int i = 0; i < str.length(); i++) {
+		    if ((int)str.charAt(i) < 48 || (int)str.charAt(i) > 57){
+		        break;
+		    }
+			result = result*10 + (str.charAt(i) - '0');
+		}
+		
+		if (signed && result > Integer.MAX_VALUE) {
+			result = Integer.MIN_VALUE; 
+		} else if (result > Integer.MAX_VALUE) {
+			result = Integer.MAX_VALUE;
+		} else if (signed) {
+			result = -result;
+		}
+		
+		return (int) result;
+	}
+
+}
--- exercises/strings/ReverseString.java
+++ exercises/strings/ReverseString.java
@@ -2,15 +2,53 @@ package strings;
 
 public class ReverseString {
 	public static void main(String[] args){
-		String myString = "America este continent";
+		String myString = "abcd";
 		System.out.println(reverse(myString));
 	}
 	
-	public static String reverse(String str) {
-	  if (str.length() == 1) {
-	    return str;
-	  }
-	  
-	  return reverse(str.substring(1)) + str.charAt(0);
+	public static String reverse(String s) {
+		if (s.length() == 0) {
+			return "";
+		}
+		
+		if (s.length() == 1) {
+			return s;
+		}
+		
+		char tmp;
+		char[] charsArray = s.toCharArray();
+		
+		int l = charsArray.length;
+		int i = 0;
+		int j = l - 1;
+		int middle = (i + j)/2;
+	
+		while (i <= middle) {
+			tmp = charsArray[i];
+			charsArray[i] = charsArray[j];
+			charsArray[j] = tmp;
+			j--;
+			i++;
+		}
+		
+		return new String(charsArray);
 	}
+	
+	public static String reverse1(String s) {
+		if (s.length() == 1) {
+			return s;
+		}
+		
+		return reverse(s.substring(1)) + s.charAt(0);
+	}
+	
+	
+	
+//	public static String reverse(String str) {
+//	  if (str.length() == 1) {
+//	    return str;
+//	  }
+//	  
+//	  return reverse(str.substring(1)) + str.charAt(0);
+//	}
 }
