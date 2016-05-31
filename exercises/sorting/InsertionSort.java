8363c3daaf29cd51282c8c8f75a0ddbe6d87b432
Ionut Pistol <ionut.pistol@gmail.com>
2016-05-31 11:02:00 +0300

    Array intersection, odd even linked list

--- exercises/sorting/InsertionSort.java
+++ exercises/sorting/InsertionSort.java
@@ -2,15 +2,40 @@ package sorting;
 
 import java.util.Random;
 
-public class InsertSort {
+public class InsertionSort {
 
 	public static void main(String[] args) {
-		int[] unsortedArray = generateArray(1000000);
+		int[] unsortedArray = generateArray(4);
 		System.out.print("Unsorted array: ");
-//		printElements(unsortedArray);
-		insertionSort(unsortedArray);
+//		insertionSort(unsortedArray);
+		int[] sortedArray = insertionSort2(unsortedArray);
 		System.out.print("Sorted array: ");
-//		printElements(unsortedArray);
+		printElements(sortedArray);
+	}
+	
+	/**
+	 * For each element:
+	 * 	- find the next index where another element greater than the current
+	 * 	- if it is found then save the current element and then replace all the elements except the current
+	 * 	- when the replacement of all elements is finished then save the current element on the new position 
+	 * 	
+	 * @param data
+	 * @return
+	 */
+	public static int[] insertionSort2(int[] data) {
+		for (int i = 0; i < data.length; i++) {
+			int j = i;
+			while (j > 0 && data[i] < data[j - 1]) {
+				j--;
+			}
+			int tmp = data[i];
+			for(int k = i; k > j; k--) {
+				data[k] = data[k - 1];
+			}
+			data[j] = tmp;
+		}
+		
+		return data;
 	}
 	
 	public static void insertionSort(int[] array) {
@@ -42,7 +67,8 @@ public class InsertSort {
 		Random rand = new Random();
 		int counter = 0;
 		int minim = 0;
-		int maxLimit = Integer.MAX_VALUE;
+//		int maxLimit = Integer.MAX_VALUE;
+		int maxLimit = 20;
 
 		do {
 			int next = getRandomInteger(minim, maxLimit, rand);
